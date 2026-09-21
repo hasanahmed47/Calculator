2225172464de1c93b75fd2c155198cf794d15ce6
Hasan Ahmed <hasanmuhammad2004@gmail.com>
2026-09-21 14:48:40 +0500

    few changes

--- app.js
+++ app.js
@@ -1,23 +1,65 @@
+let display=document.querySelector('input');
+let butt=document.querySelectorAll('.butt')
+let action=document.querySelectorAll('.action')
+let operation=document.querySelectorAll('.operator')
 
-let display=document.getElementById('input');
+let previousNumber=''
+let operator=''
+let currentNumber=''
+let result=''
 
+butt.forEach((button) =>{
+    button.addEventListener('click', (input)=>{
+       currentNumber += input.target.innerText;
+       display.value = `${previousNumber}${operator}${currentNumber}`
+    })
+})
 
-function append(item){
-    display.value += item; 
-}
+operation.forEach((op) =>{
+    op.addEventListener('click', (event)=>{
 
-function calculating(){
-  try{
-     display.value=eval(display.value);
-  }catch(error){
-    display.value="error"
-  }
-}
+        if(previousNumber !== '' && currentNumber !== ''){
+             if(operator === '+') result = `${parseFloat(previousNumber) + parseFloat(currentNumber)}`
+          if(operator === '-') result = `${parseFloat(previousNumber) - parseFloat(currentNumber)}`
+          if(operator === '*') result = `${parseFloat(previousNumber) * parseFloat(currentNumber)}`
+          if(operator === '/') result = `${parseFloat(previousNumber) / parseFloat(currentNumber)}`
+          previousNumber=result;
+          currentNumber=''
+          display.value=result
+        }else{
+            previousNumber= currentNumber; 
+        }
+         
+        operator = event.target.innerText;
+        currentNumber=''
+        display.value = `${previousNumber}${operator}`
+    })
+})
 
-function deletee(){
-display.value=display.value.slice("0","-1")
-}
+action.forEach((act)=>{
+    act.addEventListener('click', (event) =>{
+    if(event.target.innerText === '='){     
+          if(operator === '+') result = `${parseFloat(previousNumber) + parseFloat(currentNumber)}`
+          if(operator === '-') result = `${parseFloat(previousNumber) - parseFloat(currentNumber)}`
+          if(operator === '*') result = `${parseFloat(previousNumber) * parseFloat(currentNumber)}`
+          if(operator === '/') result = `${parseFloat(previousNumber) / parseFloat(currentNumber)}`
+          if(operator === '/') result = `${parseFloat(previousNumber) % parseFloat(currentNumber)}`
 
-function remove(){
-    display.value="";
-}
+          currentNumber=result;
+          previousNumber=''
+          operator=''
+          display.value=result
+    }
+    if(event.target.innerText === "AC") {
+        display.value=0
+        previousNumber=''
+        currentNumber=''
+        operator=''
+    }
+
+    if(event.target.innerText === 'DEL'){
+        display.value=display.value.slice(0,-1)
+        currentNumber=display.value
+    }
+    } )
+})
